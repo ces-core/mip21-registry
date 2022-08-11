d155bc60b6c89515caad8b961635338cddd6ac96
dbkbali <dbkbali@users.noreply.github.com>
2022-08-11 16:03:55 -0400

    chore: update pragma for dss-cron compatibility

--- src/RwaRegistry.t.sol
+++ src/RwaRegistry.t.sol
@@ -1,6 +1,6 @@
 // SPDX-FileCopyrightText: © 2022 Dai Foundation <www.daifoundation.org>
 // SPDX-License-Identifier: GPL-3.0-or-later
-pragma solidity ^0.8.14;
+pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
 import {RwaRegistry} from "./RwaRegistry.sol";

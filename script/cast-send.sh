2e1484dcb921c3d6e6ee25f01afbad170fed68b4
amusingaxl <112016538+amusingaxl@users.noreply.github.com>
2022-10-28 16:32:26 -0300

    chore: apply new formatting rules to script files

--- script/cast-send.sh
+++ script/cast-send.sh
@@ -4,27 +4,27 @@ set -eo pipefail
 source "${BASH_SOURCE%/*}/_common.sh"
 
 send() {
-  normalize-env-vars
+	normalize-env-vars
 
-  local PASSWORD="$(extract-password)"
-  if [ -n "$PASSWORD" ]; then
-    PASSWORD_OPT="--password=${PASSWORD}"
-  fi
+	local PASSWORD="$(extract-password)"
+	if [ -n "$PASSWORD" ]; then
+		PASSWORD_OPT="--password=${PASSWORD}"
+	fi
 
-  local RESPONSE
-  # Log the command being issued, making sure not to expose the password
-  log "cast send --keystore="$FOUNDRY_ETH_KEYSTORE_FILE" $(sed 's/=.*$/=[REDACTED]/' <<<"$PASSWORD_OPT") --json" $(printf ' %q' "$@")
-  # Currently `cast send` sends the logs to stdout instead of stderr.
-  # This makes it hard to compose its output with other commands, so here we are:
-  # 1. Duplicating stdout to stderr through `tee`
-  # 2. Extracting only the hash of the transaction to stdout
-  RESPONSE=$(cast send --keystore="$FOUNDRY_ETH_KEYSTORE_FILE" "$PASSWORD_OPT" --json "$@" | tee >(cat 1>&2))
+	local RESPONSE
+	# Log the command being issued, making sure not to expose the password
+	log "cast send --keystore="$FOUNDRY_ETH_KEYSTORE_FILE" $(sed 's/=.*$/=[REDACTED]/' <<<"$PASSWORD_OPT") --json" $(printf ' %q' "$@")
+	# Currently `cast send` sends the logs to stdout instead of stderr.
+	# This makes it hard to compose its output with other commands, so here we are:
+	# 1. Duplicating stdout to stderr through `tee`
+	# 2. Extracting only the hash of the transaction to stdout
+	RESPONSE=$(cast send --keystore="$FOUNDRY_ETH_KEYSTORE_FILE" "$PASSWORD_OPT" --json "$@" | tee >(cat 1>&2))
 
-  jq -r '.transactionHash' <<<"$RESPONSE"
+	jq -r '.transactionHash' <<<"$RESPONSE"
 }
 
 usage() {
-  cat <<MSG
+	cat <<MSG
 cast-send.sh <address> <method_signature> [ ...args ]
 
 Examples:
@@ -38,10 +38,10 @@ MSG
 }
 
 if [ "$0" = "$BASH_SOURCE" ]; then
-  [ "$1" = "-h" -o "$1" = "--help" ] && {
-    echo -e "\n$(usage)\n"
-    exit 0
-  }
+	[ "$1" = "-h" -o "$1" = "--help" ] && {
+		echo -e "\n$(usage)\n"
+		exit 0
+	}
 
-  send "$@"
+	send "$@"
 fi
--- script/forge-deploy.sh
+++ script/forge-deploy.sh
@@ -4,38 +4,38 @@ set -eo pipefail
 source "${BASH_SOURCE%/*}/_common.sh"
 
 deploy() {
-  normalize-env-vars
+	normalize-env-vars
 
-  local PASSWORD="$(extract-password)"
-  if [ -n "$PASSWORD" ]; then
-    PASSWORD_OPT="--password=${PASSWORD}"
-  fi
+	local PASSWORD="$(extract-password)"
+	if [ -n "$PASSWORD" ]; then
+		PASSWORD_OPT="--password=${PASSWORD}"
+	fi
 
-  check-required-etherscan-api-key
+	check-required-etherscan-api-key
 
-  local RESPONSE=
-  # Log the command being issued, making sure not to expose the password
-  log "forge create --keystore="$FOUNDRY_ETH_KEYSTORE_FILE" $(sed 's/=.*$/=[REDACTED]/' <<<"$PASSWORD_OPT") --json" $(printf ' %q' "$@")
-  # Currently `forge create` sends the logs to stdout instead of stderr.
-  # This makes it hard to compose its output with other commands, so here we are:
-  # 1. Duplicating stdout to stderr through `tee`
-  # 2. Extracting only the address of the deployed contract to stdout
-  RESPONSE=$(forge create --keystore="$FOUNDRY_ETH_KEYSTORE_FILE" "$PASSWORD_OPT" --json "$@" | tee >(cat 1>&2))
+	local RESPONSE=
+	# Log the command being issued, making sure not to expose the password
+	log "forge create --keystore="$FOUNDRY_ETH_KEYSTORE_FILE" $(sed 's/=.*$/=[REDACTED]/' <<<"$PASSWORD_OPT") --json" $(printf ' %q' "$@")
+	# Currently `forge create` sends the logs to stdout instead of stderr.
+	# This makes it hard to compose its output with other commands, so here we are:
+	# 1. Duplicating stdout to stderr through `tee`
+	# 2. Extracting only the address of the deployed contract to stdout
+	RESPONSE=$(forge create --keystore="$FOUNDRY_ETH_KEYSTORE_FILE" "$PASSWORD_OPT" --json "$@" | tee >(cat 1>&2))
 
-  jq -Rr 'fromjson? | .deployedTo' <<<"$RESPONSE"
+	jq -Rr 'fromjson? | .deployedTo' <<<"$RESPONSE"
 }
 
 check-required-etherscan-api-key() {
-  # Require the Etherscan API Key if --verify option is enabled
-  set +e
-  if grep -- '--verify' <<<"$@" >/dev/null; then
-    [ -n "$ETHERSCAN_API_KEY" ] || die "$(err-msg-etherscan-api-key)"
-  fi
-  set -e
+	# Require the Etherscan API Key if --verify option is enabled
+	set +e
+	if grep -- '--verify' <<<"$@" >/dev/null; then
+		[ -n "$ETHERSCAN_API_KEY" ] || die "$(err-msg-etherscan-api-key)"
+	fi
+	set -e
 }
 
 usage() {
-  cat <<MSG
+	cat <<MSG
 forge-deploy.sh [<file>:]<contract> [ --verify ] [ --constructor-args ...args ]
 
 Examples:
@@ -49,10 +49,10 @@ MSG
 }
 
 if [ "$0" = "$BASH_SOURCE" ]; then
-  [ "$1" = "-h" -o "$1" = "--help" ] && {
-    echo -e "\n$(usage)\n"
-    exit 0
-  }
+	[ "$1" = "-h" -o "$1" = "--help" ] && {
+		echo -e "\n$(usage)\n"
+		exit 0
+	}
 
-  deploy "$@"
+	deploy "$@"
 fi
